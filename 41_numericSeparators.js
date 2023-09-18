a9a31a5d9a92564c5bc65a901967f84b475157ac
Pawan <pawanptl34@gmail.com>
2023-09-18 11:40:18 +0530

    numeric separators

---
+++ 41_numericSeparators.js
@@ -0,0 +1,14 @@
+// Numeric Separators
+// To make numbers readable
+
+const diameter = 287_460_000_000;
+console.log(diameter);
+
+const priceCents = 345_99;
+console.log(priceCents);
+
+const PI = 3.14_15;
+console.log(PI);
+
+console.log(Number("53654_623")); // NaN
+console.log(parseInt("5654_898")); // 5654
